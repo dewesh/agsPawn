f280b8a65d98cbb7fbbd1311c963a0a5ad864350
dewesh agrawal <dagrawal@walmartlabs.com>
2018-03-25 19:17:49 +0530

    init project

---
+++ src/main/java/com/ags/pawn/services/agsPawnService/AgsPawnServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ags.pawn.services.agsPawnService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgsPawnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgsPawnServiceApplication.class, args);
+	}
+}
